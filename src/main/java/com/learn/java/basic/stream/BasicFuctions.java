867302eb90451adf447f99129a475b43bf9eef87
tatKen <lurkkents@gmail.com>
2022-03-31 22:01:49 +0800

    first commit

--- src/main/java/com/learn/java/basic/stream/BasicFuctions.java
+++ src/main/java/com/learn/java/basic/stream/BasicFuctions.java
@@ -1,4 +1,22 @@
 package com.learn.java.basic.stream;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class BasicFuctions {
+    public static void main(String []args){
+        Predicate<String> matchStr = x -> x.equalsIgnoreCase("two");
+        Predicate<String> excludeStr = x -> (!x.equalsIgnoreCase("four"));
+
+        Stream<String> dummyStream = Stream.empty();
+
+        if(dummyStream.count() == 0){
+            dummyStream = Stream.of("One", "Two", "Third");
+        }
+
+        dummyStream.forEach((t) -> String.format("append the String with %s", t));
+
+        dummyStream.forEach((t) -> System.out.println(t));
+    }
+
 }
--- src/test/java/com/learn/java/basic/IOTesting.java
+++ src/test/java/com/learn/java/basic/IOTesting.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class IOTesting {
-    public static void main(){
-        Path orgPath = Files.createFile();
+    public static void main(String[] args){
+//        Path orgPath = Files.createFile();
     }
 }
